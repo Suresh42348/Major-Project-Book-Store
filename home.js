758f9ab3e8adc548e8a184bfe317f9ba7fef2a2f
Suresh42348 <panku42348@gmail.com>
2022-02-28 00:35:41 +0530

    darkmode

---
+++ home.js
@@ -0,0 +1,19 @@
+var x=1;
+function changeMode(){
+    var mybody = document.body;
+    mybody.classList.toggle('mydark');
+   
+    document.getElementsByClassName('social')[0].style.backgroundColor="black";
+    document.getElementsByClassName('footer')[0].style.backgroundColor="black";
+    document.getElementsByClassName('footer')[0].style.color="white";
+    var element=document.getElementsByClassName('icon');
+    for (var i = 0; i < element.length; i++) {
+        element[i].style.backgroundColor="white";
+        }
+
+
+}
+
+
+
+
