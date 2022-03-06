ce30748f5f4fe5bcd122e009c9f79c314fd248f0
Suresh42348 <panku42348@gmail.com>
2022-03-06 18:40:21 +0530

    weather

--- home.js
+++ home.js
@@ -3,9 +3,6 @@ function changeMode(){
     var mybody = document.body;
     mybody.classList.toggle('mydark');
    
-    document.getElementsByClassName('social')[0].style.backgroundColor="black";
-    document.getElementsByClassName('footer')[0].style.backgroundColor="black";
-    document.getElementsByClassName('footer')[0].style.color="white";
     var element=document.getElementsByClassName('icon');
     for (var i = 0; i < element.length; i++) {
         element[i].style.backgroundColor="white";
